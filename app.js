23c2ac6e5f1d658e7750e8748e522e158831ca33
Romain <rmtestard@gmail.com>
2018-12-09 11:48:23 +0100

    refactoring into a controller bookRoutes

--- app.js
+++ app.js
@@ -3,46 +3,15 @@ const express = require('express'),
   bodyParser = require('body-parser');
 
 const app = express();
-const bookRouter = express.Router();
-
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const Book = require('./models/bookModel');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+bookRouter = require('./Routes/bookRoutes')();
 app.use('/api', bookRouter);
-
-bookRouter
-  .route('/Books')
-  .post(function(req, res) {
-    var book = new Book(req.body);
-    book.save();
-    res.status(201).send(book);
-  })
-  .get(function(req, res) {
-    var query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, function(err, books) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.json(books);
-      }
-    });
-  });
-
-bookRouter.route('/Books/:bookid').get(function(req, res) {
-  Book.findById(req.params.bookid, function(err, book) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(book);
-    }
-  });
-});
 
 app.get('/', (req, res) => {
   res.send('welcome to my API!');
